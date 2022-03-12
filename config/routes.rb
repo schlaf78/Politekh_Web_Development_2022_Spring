54c56436d671bc76b45912ad5098b6c41c1eb8cf
m.burmistrov <schlaf78@gmail.com>
2022-03-13 00:10:11 +0300

    Lab 1 complete + fix problem error "rake routes rake aborted!" with:
    -Open your project folder
    -Navigate to lib/tasks
    -Create file called routes.rake
    copypaste text below:
    
    task routes: :environment do
      puts `bundle exec rails routes`
    end

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'main/index'
+  get 'main/help'
+  get 'main/contacts'
+  get 'main/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
